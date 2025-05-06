1c0c6f4f29a90c946cb1599d0f700aebaba17541
M A C W I N <mavzurovnurali@gmail.com>
2025-05-06 14:15:26 +0500

    sss

--- While03.py
+++ While03.py
@@ -1,9 +1,22 @@
-def main(s):
+import string
+def count_punctuation(s):
     """
     A variable of type str is given. Find how many punctuations it contains and return.
     Args:
         s: str
     Returns:
         int: return answer
+
     """
-    return
+    i=0
+    count=0
+    while i<len(s):
+        if s[i] in string.punctuation:
+            count+=1
+            
+        i+=1
+        
+    return count 
+
+s="heloo, world.,;;"
+print(count_punctuation(s))
