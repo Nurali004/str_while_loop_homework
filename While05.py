df9b53429c231071a68d62c672f04c1538485092
M A C W I N <mavzurovnurali@gmail.com>
2025-05-06 14:32:53 +0500

    hhh

--- While05.py
+++ While05.py
@@ -1,4 +1,4 @@
-def main(s):
+def count_lowercase(s):
     """
     A variable of type str is given. Find how many lowercase letters there are and return.
     Args:
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    count=0
+    i=0
+    while i<len(s):
+        if s[i].islower():
+            count+=1
+        i+=1
+    return count
+
+s="ndfhjshjfsjh"
+print(count_lowercase(s))
