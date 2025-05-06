d2f7409e65aa0d6fc32d85db27a35cc25fef090c
M A C W I N <mavzurovnurali@gmail.com>
2025-05-06 14:46:26 +0500

    ggg

--- While06.py
+++ While06.py
@@ -6,4 +6,18 @@ def main(s):
         consonant: other than vowels(a, e, i, o, u)
     Returns:
         int: return answer
+
+
     """
+    vowels="aeiouAEIOU" 
+    count =0
+    i=0
+    while i<len(s):
+        if s[i].isalpha():
+            count+=1
+        i+=1
+
+        return count
+
+s="hello world"
+print(main(s))
