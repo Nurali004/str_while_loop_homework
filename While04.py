197f14c8980b233046169a7fe9b10d9e4a819dbc
M A C W I N <mavzurovnurali@gmail.com>
2025-05-06 14:28:58 +0500

    ddd

--- While04.py
+++ While04.py
@@ -1,9 +1,19 @@
-def main(s):
+def count_uppercase(s):
     """
     A variable of type str is given. Find how many uppercase letters there are and return.
     Args:
         s: str
     Returns:
         int: return answer
+
     """
-    return
+    count=0
+    i=0
+    while i<len(s):
+        if s[i].isupper():
+            count+=1
+        i+=1
+    return count
+
+s="Hello Teacher"
+print(count_uppercase(s))
