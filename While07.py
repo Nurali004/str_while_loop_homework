d29d19ad1a51db0751cf49a1c21ab2dcf0854a08
M A C W I N <mavzurovnurali@gmail.com>
2025-05-06 14:49:12 +0500

    ggg

--- While07.py
+++ While07.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    count=0
+    i=0
+    while i<len(s):
+        if s[i].isdigit():
+            count+=1
+        i+=1
+
+    return count
+s="fehjerghhg1234"
+print(main(s))
